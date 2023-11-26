c992da0ccc590e33b9c76e49cb2ab77a9e5fbc01
Chack96 <francescoloda96@gmail.com>
2023-11-26 14:59:11 +0100

    Implemented basic routing with timer

--- NavigationPage.tsx
+++ NavigationPage.tsx
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { View, ScrollView, TextInput, Button, StyleSheet, Text, Alert } from 'react-native';
 import {NavigationPageScreenProps} from './NavigationTypes'
-import { Camera, CameraDevice, useCameraDevice, useCameraPermission, useCodeScanner } from "react-native-vision-camera"
+import { Camera, CameraDevice, useCameraDevice, useCameraPermission, useCodeScanner, Code } from "react-native-vision-camera"
 import {showPermissionAlert, showDeviceAlert} from './Alert'
+import {Stopwatch} from './Stopwatch'
 
 export function NavigationPage({ navigation }: NavigationPageScreenProps): JSX.Element {
     const { hasPermission, requestPermission } = useCameraPermission();
     const [cameraIsVisible, setCameraIsVisible] = useState(false);
-    
+    const [isCounting, setIsCounting] = useState(false);
+    const [isStopwatchVisible, setIsStopwatchVisible] = useState(false);
+    const [isStartButtonVisible, setIsStartButtonVisible] = useState(true);
+    const [buttonTitle, setButtonTitle] = useState('Start Route');
+    const [isRouting, setIsRouting] = useState(false);
+
     // First thing happening upon entering the page
     // I tried doing it after starting the route but
     // For some reasons it didn't returned the value even with an async function
-    if (!hasPermission) {
-        requestPermission();
-    }
+    useEffect(() => {
+        if (!hasPermission) {
+            requestPermission();
+        }
+    }, []);
 
+    // For some reason in the useEffect the code doesn't run
     let device: CameraDevice | undefined;
     const devices = Camera.getAvailableCameraDevices();
     if (devices !== null) {
@@ -25,21 +34,39 @@ export function NavigationPage({ navigation }: NavigationPageScreenProps): JSX.E
 
     const codeScanner = useCodeScanner({
     codeTypes: ['qr'],
-    onCodeScanned: (codes) => {
+    onCodeScanned: (codes) => {VerifyCode(codes)}
+    });
+
+    function VerifyCode(codes: Code[]) {
         for (const code of codes) {
-        console.log('QR Code value : ' + code.value)
-        if (code.value === 'Starting Route') {
-            setCameraIsVisible(false);
-            console.log('Routing')
-        } /*else if (code.value === 'Starting Route') {
-            console.log('Not Routing')
-        }*/
+            console.log('QR Code value : ' + code.value)
+            if (code.value === 'Starting Route' && !(isRouting)) {
+                setCameraIsVisible(false);
+                setButtonTitle('End Route')
+                setIsStartButtonVisible(true);
+                setIsStopwatchVisible(true);
+                setIsCounting(true);
+                setIsRouting(true);
+                console.log('Routing')
+            }
+            if (code.value === 'Starting Route' && isRouting) {
+                setCameraIsVisible(false);
+                setButtonTitle('Start Route')
+                setIsStartButtonVisible(true);
+                setIsStopwatchVisible(false);
+                setIsCounting(false);
+                setIsRouting(false);
+                console.log('Ended Routing')
+            }
+
         }
-    }});
+    }
 
     function StartRoute () {
         if (hasPermission && device !== undefined) {
             setCameraIsVisible(true);
+            setIsStartButtonVisible(false);
+            setIsStopwatchVisible(false);
         } else if (!hasPermission) {
             showPermissionAlert();
         } else if (device === undefined) {
@@ -48,18 +75,21 @@ export function NavigationPage({ navigation }: NavigationPageScreenProps): JSX.E
     }
 
     return (
-        <ScrollView>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         { (cameraIsVisible && device !== undefined) && 
+        <>
             <Camera
-            style={{ width: 500, height: 800 }}
-            //style={StyleSheet.absoluteFill}
+            //style={{ width: 500, height: 800 }}
+            style={StyleSheet.absoluteFill}
             device={device}
             isActive={true}
             codeScanner={codeScanner}
             />
+        </>
         }
-        <Button onPress={() => StartRoute()} title="Start Route" />
-      </ScrollView>
+        {isStopwatchVisible && <Stopwatch isCounting={isCounting}></Stopwatch>}
+        {isStartButtonVisible && <Button onPress={() => StartRoute()} title={buttonTitle} />}
+      </View>
     );
   };
   
---
+++ Stopwatch.tsx
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect } from 'react';
+import { Text } from "react-native";
+
+interface StopwatchProps {
+    isCounting: boolean;
+  }
+
+export const Stopwatch: React.FC<StopwatchProps> = ({isCounting}) => {
+  const [startTime, setStartTime] = useState(Date.now());
+  const [now, setNow] = useState(Date.now());
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+useEffect(() => {
+    if (isCounting) {
+        handleStart();
+    } else {
+        handleStop();
+    }
+    }, [isCounting]);
+
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    if (intervalRef.current !== null) {
+        clearInterval(intervalRef.current);
+      }
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    if (intervalRef.current !== null) {
+        clearInterval(intervalRef.current);
+      }
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  return (
+    <Text style={{color: 'black'}}>Time passed: {secondsPassed.toFixed(3)}</Text> 
+  );
+};
+
+export default Stopwatch;
