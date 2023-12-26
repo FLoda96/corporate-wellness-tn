fa8495f759358eef923c145296554b28a0e7a7cf
Chack96 <francescoloda96@gmail.com>
2023-12-26 18:17:10 +0100

    Bandaid fix for the slow loading navigation page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {useState, useContext, useEffect} from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Alert, ActivityIndicator } from 'react-native';
 
 import { useTranslation } from 'react-i18next';
 import './Languages/i18n'
@@ -14,9 +14,11 @@ import {NavigationPage} from './Pages/NavigationPage'
 import {AboutUsPage} from './Pages/AboutUsPage'
 import {MainPage} from './Pages/MainPage'
 import {TeamPage} from './Pages/TeamPage'
+import {NavigationLoadingPage} from './Pages/NavigationLoadingPage'
 import {UserContext, LoginContext, UserIdContext} from './Utils/AuthContext'
 import {retrieveSessionData} from './Utils/EncryptedStorageUtility'
 import { HandleLogin, sessionAuthName } from './Utils/FunctionUtils';
+import { LoadingScreen } from './Utils/LoadingScreen';
 
 
 // TO DO : Save the language in the storage to recover the preference
@@ -24,6 +26,7 @@ import { HandleLogin, sessionAuthName } from './Utils/FunctionUtils';
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [userId, setUserId] = useState(0);
   const { t, i18n } = useTranslation();
   const toggleRememberDataPlaceholder = false;
@@ -55,6 +58,17 @@ export default function App() {
     // Roundabout way to call the above but async TO DO : search a library that does this
     fetchData();
   }, []);
+
+  const loading_notice = t('alerts.loading_notice');
+  const loading_alert = t('alerts.loading_alert');
+
+    function showLoadingAlert () {
+        Alert.alert(
+        loading_notice,
+        loading_alert,
+        [{ text: 'Ok', style: 'default',}]);
+    }
+
   // options={{ headerTitle : () =>  <Button title='Disconnect'/>}}
   // TO DO : There must be a way to put the option in some style file, probably some TypeScript annoyance
   return (
@@ -63,13 +77,16 @@ export default function App() {
       <UserIdContext.Provider value={{UserId : userId, SetUserId : setUserId}}>
       <LoginContext.Provider value={{IsAuthenticated : isAuthenticated, SetIsAuthenticated : setIsAuthenticated}}>
       {isAuthenticated ? (
+        <>
         <HomeDrawer.Navigator initialRouteName="Main" screenOptions={{headerStyle: {backgroundColor: '#11ab7a',}, headerTintColor: '#fff', headerTitleStyle: {fontWeight: 'bold',}}}>
           <HomeDrawer.Screen name="Profile" component={ProfilePage} options={{ title: profile}} />
           <HomeDrawer.Screen name="Teams" component={TeamPage} options={{ title: teams}} />
           <HomeDrawer.Screen name="AboutUs" component={AboutUsPage} options={{ title: about_us}} />
           <HomeDrawer.Screen name="Main" component={MainPage} options={{ title: main_page}} />
-          <HomeDrawer.Screen name="Navigation" component={NavigationPage} options={{ title: navigation}} />
+          <HomeDrawer.Screen name="Navigation" component={NavigationPage} options={{ title: navigation}} listeners={{drawerItemPress: (e) => {showLoadingAlert()}}} />
+          {/*<HomeDrawer.Screen name="LoadingNavigation" component={NavigationLoadingPage} options={{ title: "navigation_load"}} />*/}
         </HomeDrawer.Navigator>
+        </>
       ) : (
         <AuthStack.Navigator initialRouteName="Register" screenOptions={{headerStyle: {backgroundColor: '#11ab7a',}, headerTintColor: '#fff', headerTitleStyle: {fontWeight: 'bold',}}}>
           <AuthStack.Screen name="Register" component={RegisterPage} options={{ title: register}} />
